a3bc561316020793ddef18d777adde6922f83170
artra <radkevich.as@esphere.ru>
2024-01-29 00:47:36 +0300

    run auth server in docker compose

--- db/liquibase/run-liquibase.sh
+++ db/liquibase/run-liquibase.sh
@@ -5,5 +5,7 @@ LQ_PASSWORD=${POSTGRES_PASSWORD:-postgres}
 LQ_URL=${POSTGRES_URL:-jdbc:postgresql://postgresql:5432/farel}
 
 /liquibase/liquibase --driver=org.postgresql.Driver --url=$LQ_URL --changeLogFile=./scripts/init.sql --username=$LQ_USER --password=$LQ_PASSWORD update
+/liquibase/liquibase --driver=org.postgresql.Driver --url=$LQ_URL --changeLogFile=./changelog/changelog-1.xml --username=$LQ_USER --password=$LQ_PASSWORD generate-changelog
 /liquibase/liquibase --driver=org.postgresql.Driver --url=$LQ_URL --changeLogFile=./scripts/data.sql --username=$LQ_USER --password=$LQ_PASSWORD update
-/liquibase/liquibase --driver=org.postgresql.Driver --url=$LQ_URL --changeLogFile=./scripts/changelog-2.xml --username=$LQ_USER --password=$LQ_PASSWORD generate-changelog
+/liquibase/liquibase --driver=org.postgresql.Driver --url=$LQ_URL --changeLogFile=./changelog/changelog-2.xml --username=$LQ_USER --password=$LQ_PASSWORD generate-changelog
+
